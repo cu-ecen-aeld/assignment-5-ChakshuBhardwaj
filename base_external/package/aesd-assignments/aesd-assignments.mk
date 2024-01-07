7b1c1c94d6b019f608b8884bb054ecfe6a4f546d
Chakshu Bhardwaj <chakshu.bhardwaj93@gmail.com>
2024-01-06 19:37:03 -0500

    Commiting after successful run of full-test.sh

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,11 +6,11 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '5367942'
+AESD_ASSIGNMENTS_VERSION = '3daad9d'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecec-aeld/assignments-3-and-later-ChakshuBhardwaj.git'
+AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-ChakshuBhardwaj.git'
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
